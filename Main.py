724f8b06d95df63506cdd94f6b52407e0bec01f0
unknown <denik.88@yandex.ru>
2020-05-05 22:38:23 +0300

    added visibiliti values on interface

--- Main.py
+++ Main.py
@@ -2,15 +2,23 @@ from logics import *
 import pygame
 import sys
 
-# План работ
-# Положить в массив два значения
-# Начал цикл игры:
-#    ждать от пользователя команды
-#    когда получим команду обработать массив
-#    найти пустые клетки
-#    если есть пустые клетки случайно выбрать одну из них
-#    и положить туда 2 либо 4
-#    если пустых клеток нет и нельзя двигать массив, игра закончена
+def draw_interface():
+    font = pygame.font.SysFont("stxingkai", 70)
+    pygame.draw.rect(screen, WHITE_COLOR, TITLE_RECTANGLE)
+    pretty_print(mas)
+    for row in range(BLOCKS):
+        for column in range(BLOCKS):
+            value = mas[row][column]  # Вместе с отрисовкой квадратов узнаю значение по этому индексу
+            text = font.render(f'{value}', True, BLACK_COLOR)
+            w = column * BLOCK_SIZE + (column + 1) * MARGIN  # Точка  отрисовки квадрата по высоте
+            h = row * BLOCK_SIZE + (row + 1) * MARGIN + 110  # По ширине
+            pygame.draw.rect(screen, COLORS[value], (w, h, 110, 110))
+            if value != 0:
+                font_w, font_h = text.get_size()  # Узнал ширину и высоту текста
+                text_x = w + (BLOCK_SIZE - font_w) // 2
+                text_y = h + ((BLOCK_SIZE - font_h) // 2)
+                screen.blit(text, (text_x, text_y))
+
 
 mas = [
     [0, 0, 0, 0],
@@ -19,8 +27,18 @@ mas = [
     [0, 0, 0, 0]
 ]
 
-GRAY = (130, 130, 130)
-COLOR = (255, 255, 255)
+COLORS = {
+    0: (130, 130, 130),
+    2: (255, 255, 255),
+    4: (255, 255, 130),
+    8: (255, 255, 0),
+    16: (255, 130, 255)
+}
+
+color = (0, 0, 0)
+BLACK_COLOR = (0, 0, 0)
+GRAY_COLOR = (130, 130, 130)
+WHITE_COLOR = (255, 255, 255)
 BLOCKS = 4
 MARGIN = 10
 BLOCK_SIZE = 110
@@ -37,24 +55,23 @@ pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("2048")
 
+draw_interface()
+pygame.display.update()
+
 while is_zero_in_mas(mas):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit(0)
         elif event.type == pygame.KEYDOWN:
-            pygame.draw.rect(screen, COLOR, TITLE_RECTANGLE)
-            for row in range(BLOCKS):
-                for column in range(BLOCKS):
-                    w = column * BLOCK_SIZE + (column+1) * MARGIN
-                    h = row * BLOCK_SIZE + (row+1) * MARGIN + 110
-                    pygame.draw.rect(screen, GRAY, (w, h, 110, 110))
-            # input()
+            font = pygame.font.SysFont("stxingkai", 70)
+            pygame.draw.rect(screen, WHITE_COLOR, TITLE_RECTANGLE)
+            pretty_print(mas)
             empty = get_empty_list(mas)  # Получил список пустых элементов
             random.shuffle(empty)  # Перемешал список
             random_num = empty.pop()  # Удалил и взял последний элемент с конца списка.
             i, j = get_index_from_number(random_num)  # Получил индексы элемента из его номера
             mas = insert_2_or_4(mas, i, j)
             print(f'Мы заполнили элемент под номером {random_num}')
-            pretty_print(mas)
-        pygame.display.update()
+            draw_interface()
+            pygame.display.update()
