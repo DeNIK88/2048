8fbaa7462e75b694d2acf40a4463ad37e7aea62c
unknown <denik.88@yandex.ru>
2020-05-21 22:40:48 +0300

    added  restart game with name or wichout name. Press spase or enter

--- Main.py
+++ Main.py
@@ -3,8 +3,43 @@ import pygame
 import sys
 from database import get_best, cur, insert_result
 
-GAMERS_FROM_DATABASE = get_best()
-USERNAME = None
+def game_loop():
+    global score, mas
+    draw_interface(score)
+    pygame.display.update()
+
+    while is_zero_in_mas(mas) or can_movie(mas):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit(0)
+            elif event.type == pygame.KEYDOWN:
+                delta = 0
+                if event.key == pygame.K_LEFT:
+                    mas, delta = move_left(mas)
+                elif event.key == pygame.K_RIGHT:
+                    mas, delta = move_right(mas)
+                elif event.key == pygame.K_UP:
+                    mas, delta = move_up(mas)
+                elif event.key == pygame.K_DOWN:
+                    mas, delta = move_down(mas)
+                score += delta
+                font = pygame.font.SysFont("stxingkai", 70)
+                pygame.draw.rect(screen, WHITE_COLOR, TITLE_RECTANGLE)
+                if is_zero_in_mas(mas):
+                    empty = get_empty_list(mas)  # Беру список пустых элементов
+                    random.shuffle(empty)  # Перемешал перемешиваю его
+                    random_num = empty.pop()  # Удалил и взял последний элемент с конца списка.
+                    # Метод поп возвращает взятый элемент.
+                    i, j = get_index_from_number(random_num)  # Получил индексы элемента из его номера
+                    mas = insert_2_or_4(mas, i, j)  # Поставил на место взятого пустого элемента 2 или 4
+                    print(f'Мы заполнили элемент под номером {random_num}')
+                draw_interface(score, delta)
+                pygame.display.update()
+        print(USERNAME)  # Проверка что имя введено то которое было введено пользователем
+
+    game_over()
+
 
 def draw_intro():
     img = pygame.image.load("2048img.png")
@@ -47,6 +82,7 @@ def draw_intro():
     screen.fill(BLACK_COLOR)
 
 def game_over():
+    global USERNAME, mas, score
     img = pygame.image.load("2048img.png")
     font = pygame.font.SysFont("stxingkai", 60)
     font_record = pygame.font.SysFont("stxingkai", 50)
@@ -59,17 +95,31 @@ def game_over():
     else:
         text_record = font_record.render(f"Рекорд не побит: {best_score}", True, WHITE_COLOR)
     insert_result(USERNAME, score)
-    while True:
+    make_disicion = False
+    while not make_disicion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit(0)
+            elif event.type == pygame.KEYDOWN:
+
+                if event.key == pygame.K_SPACE:
+                    # restart game with name
+                    make_disicion = True
+                    init_const()
+                elif event.key == pygame.K_RETURN:
+                    # restart game without name
+                    USERNAME = None
+                    make_disicion = True
+                    init_const()
+
         screen.fill(BLACK_COLOR)
         screen.blit(text_game_over, (250, 80))
         screen.blit(text_score, (30, 250))
         screen.blit(text_record, (30, 300))
         screen.blit(img, [10, 10])
         pygame.display.update()
+    screen.fill(BLACK_COLOR)
 
 def draw_top_gamers():
     font_top = pygame.font.SysFont("simsun", 30) # Шрифт счетчика
@@ -116,16 +166,29 @@ def draw_interface(score, delta=0):
                 screen.blit(text, (text_x, text_y))
 
 
-mas = [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0]
-]
+def init_const():
+    global score, mas
+    mas = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ]
+    score = 0
+    empty = get_empty_list(mas)  # Беру список пустых элементов
+    random.shuffle(empty)  # Перемешал перемешиваю его
+    random_num1 = empty.pop()  # Удалил и взял последний элемент с конца списка.
+    # Метод поп возвращает взятый элемент.
+    random_num2 = empty.pop()
+    x1, y1 = get_index_from_number(random_num1)  # Получил индексы элемента из его номера
+    mas = insert_2_or_4(mas, x1, y1)  # Поставил на место взятого пустого элемента 2 или 4
+    x2, y2 = get_index_from_number(random_num2)
+    mas = insert_2_or_4(mas, x2, y2)
 
+mas = None
+score = None
+init_const()
 COLOR_SCORE = (255, 127, 0)
-score = 0
-
 COLORS = {
     0: (130, 130, 130),
     2: (130, 255, 255),
@@ -149,47 +212,18 @@ BLOCK_SIZE = 110
 WIDTH = BLOCKS*BLOCK_SIZE + (BLOCKS+1)*MARGIN
 HEIGHT = WIDTH + BLOCK_SIZE
 TITLE_RECTANGLE = pygame.Rect(0, 0, WIDTH, BLOCK_SIZE)
+GAMERS_FROM_DATABASE = get_best()
+USERNAME = None
 
-
-mas = insert_2_or_4(mas, 1, 2)
-mas = insert_2_or_4(mas, 3, 0)
 print(get_empty_list(mas))
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("2048")
-draw_intro()
-draw_interface(score)
-pygame.display.update()
 
-while is_zero_in_mas(mas) or can_movie(mas):
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit(0)
-        elif event.type == pygame.KEYDOWN:
-            delta = 0
-            if event.key == pygame.K_LEFT:
-                mas, delta = move_left(mas)
-            elif event.key == pygame.K_RIGHT:
-                mas, delta = move_right(mas)
-            elif event.key == pygame.K_UP:
-                mas, delta = move_up(mas)
-            elif event.key == pygame.K_DOWN:
-                mas, delta = move_down(mas)
-            score += delta
-            font = pygame.font.SysFont("stxingkai", 70)
-            pygame.draw.rect(screen, WHITE_COLOR, TITLE_RECTANGLE)
-            if is_zero_in_mas(mas):
-                empty = get_empty_list(mas)  # Беру список пустых элементов
-                random.shuffle(empty)  # Перемешал перемешиваю его
-                random_num = empty.pop()  # Удалил и взял последний элемент с конца списка.
-                # Метод поп возвращает взятый элемент.
-                i, j = get_index_from_number(random_num)  # Получил индексы элемента из его номера
-                mas = insert_2_or_4(mas, i, j)  # Поставил на место взятого пустого элемента 2 или 4
-                print(f'Мы заполнили элемент под номером {random_num}')
-            draw_interface(score, delta)
-            pygame.display.update()
-    print(USERNAME)     # Проверка что имя введено то которое было введено пользователем
+while True:
+    if USERNAME is None:
+        draw_intro()
+    game_loop()
+    game_over()
 
-game_over()
